b34d9e43c913abe1c2701ff7b7f6d8c03a21b67a
KrAk2007 <krakshit2006@gmail.com>
2026-04-15 10:17:48 +0530

    Initial Commit

---
+++ src/main/java/org/example/CurrentYear.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class CurrentYear {
+    public static void main(String[] args) {
+        String name = "Harry";
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+        System.out.println(name + "'s age in 2024 is " + age);
+    }
+}
+
